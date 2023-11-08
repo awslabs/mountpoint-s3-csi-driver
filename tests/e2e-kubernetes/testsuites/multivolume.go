83cb172bc03d95837da98aed7dc7f8a98cb42a9d
Vladislav Volodkin <vlaad@amazon.co.uk>
2023-11-08 17:44:03 +0000

    add test for using 2 volumes from 1 pod

--- tests/e2e-kubernetes/testsuites/multivolume.go
+++ tests/e2e-kubernetes/testsuites/multivolume.go
@@ -18,15 +18,15 @@ package custom_testsuites
 
 import (
 	"context"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"math/rand"
 	"path/filepath"
+	"time"
 
 	"github.com/onsi/ginkgo/v2"
 	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/kubernetes/test/e2e/framework"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
@@ -80,25 +80,23 @@ func (t *s3CSIMultiVolumeTestSuite) DefineTests(driver storageframework.TestDriv
 		}
 		framework.ExpectNoError(errors.NewAggregate(errs), "while cleanup resource")
 	}
+	toWrite := 1024 // 1KB
 	testTwoPodsSameVolume := func(ctx context.Context, pvc *v1.PersistentVolumeClaim, requiresSameNode bool) {
 		var pods []*v1.Pod
 		node := l.config.ClientNodeSelection
-		cs := f.ClientSet
 		// Create each pod with pvc
 		for i := 0; i < 2; i++ {
 			index := i + 1
 			ginkgo.By(fmt.Sprintf("Creating pod%d with a volume on %+v", index, node))
-			pod, err := e2epod.CreatePod(ctx, cs, f.Namespace.Name, nil, []*v1.PersistentVolumeClaim{pvc}, admissionapi.LevelBaseline, "")
+			pod, err := e2epod.CreatePod(ctx, f.ClientSet, f.Namespace.Name, nil, []*v1.PersistentVolumeClaim{pvc}, admissionapi.LevelBaseline, "")
 			framework.ExpectNoError(err)
 			// The pod must get deleted before this function returns because the caller may try to
 			// delete volumes as part of the tests. Keeping the pod running would block that.
 			// If the test times out, then the namespace deletion will take care of it.
 			defer func() {
-				framework.ExpectNoError(e2epod.DeletePodWithWait(ctx, cs, pod))
+				framework.ExpectNoError(e2epod.DeletePodWithWait(ctx, f.ClientSet, pod))
 			}()
-			pod, err = cs.CoreV1().Pods(pod.Namespace).Get(ctx, pod.Name, metav1.GetOptions{})
 			pods = append(pods, pod)
-			framework.ExpectNoError(err, fmt.Sprintf("get pod%d", index))
 			actualNodeName := pod.Spec.NodeName
 			// Set affinity for the next pod depending on requiresSameNode
 			if requiresSameNode {
@@ -109,18 +107,42 @@ func (t *s3CSIMultiVolumeTestSuite) DefineTests(driver storageframework.TestDriv
 		}
 
 		path := "/mnt/volume1"
+
 		pod1WritesTo := filepath.Join(path, "file1.txt")
+		seed := time.Now().UTC().UnixNano()
+		checkWriteToPath(f, pods[0], pod1WritesTo, toWrite, seed)
+		checkReadFromPath(f, pods[1], pod1WritesTo, toWrite, seed)
+
 		pod2WritesTo := filepath.Join(path, "file2.txt")
-		pod1Wrote := checkWriteToPath(f, pods[0], pod1WritesTo)
-		checkReadFromPath(f, pods[1], pod1WritesTo, pod1Wrote)
-		pod2Wrote := checkWriteToPath(f, pods[1], pod2WritesTo)
-		checkReadFromPath(f, pods[0], pod2WritesTo, pod2Wrote)
+		seed = time.Now().UTC().UnixNano()
+		checkWriteToPath(f, pods[1], pod2WritesTo, toWrite, seed)
+		checkReadFromPath(f, pods[0], pod2WritesTo, toWrite, seed)
+	}
+
+	testConcurrentAccessToSingleVolume := func(ctx context.Context, pvcs []*v1.PersistentVolumeClaim, seed int64, doWrite bool) {
+		node := l.config.ClientNodeSelection
+		ginkgo.By(fmt.Sprintf("Creating pod with a volume on %+v", node))
+		pod, err := e2epod.CreatePod(ctx, f.ClientSet, f.Namespace.Name, nil, pvcs, admissionapi.LevelBaseline, "")
+		framework.ExpectNoError(err)
+		defer func() {
+			framework.ExpectNoError(e2epod.DeletePodWithWait(ctx, f.ClientSet, pod))
+		}()
+		for i := 0; i < len(pvcs); i++ {
+			fileInVol := fmt.Sprintf("/mnt/volume%d/file.txt", i+1)
+			volSeed := seed + int64(i)
+			if doWrite {
+				ginkgo.By(fmt.Sprintf("Checking write to volume #%d", i))
+				checkWriteToPath(f, pod, fileInVol, toWrite, volSeed)
+			}
+			ginkgo.By(fmt.Sprintf("Checking read from volume #%d", i))
+			checkReadFromPath(f, pod, fileInVol, toWrite, volSeed)
+		}
 	}
 
 	// This tests below configuration:
 	// [pod1] [pod2]
 	// [   node1   ]
-	//   \      /     <- same volume mode
+	//   \      /
 	//   [volume1]
 	ginkgo.It("should concurrently access the single volume from pods on the same node", func(ctx context.Context) {
 		init(ctx)
@@ -135,7 +157,7 @@ func (t *s3CSIMultiVolumeTestSuite) DefineTests(driver storageframework.TestDriv
 	// This tests below configuration:
 	//        [pod1] [pod2]
 	// [   node1   ] [   node2   ]
-	//         \      /     <- same volume mode
+	//         \      /
 	//         [volume1]
 	ginkgo.It("should concurrently access the single volume from pods on different node", func(ctx context.Context) {
 		init(ctx)
@@ -146,11 +168,39 @@ func (t *s3CSIMultiVolumeTestSuite) DefineTests(driver storageframework.TestDriv
 		l.resources = append(l.resources, resource)
 		testTwoPodsSameVolume(ctx, resource.Pvc, true)
 	})
+
+	// This tests below configuration:
+	//          [pod1]                            same node       [pod2]
+	//      [   node1   ]                           ==>        [   node1   ]
+	//          /    \						                      /    \
+	//   [volume1]  [volume2]                              [volume1]  [volume2]
+	//		/				\								/				\
+	// 	[bucket1]		[bucket2]						[bucket1]		[bucket2]
+	ginkgo.It("should access to two volumes with the same volume mode and retain data across pod recreation on the same node", func(ctx context.Context) {
+		init(ctx)
+		ginkgo.DeferCleanup(cleanup)
+
+		var pvcs []*v1.PersistentVolumeClaim
+		numVols := 2
+
+		for i := 0; i < numVols; i++ {
+			testVolumeSizeRange := t.GetTestSuiteInfo().SupportedSizeRange
+			resource := storageframework.CreateVolumeResource(ctx, driver, l.config, pattern, testVolumeSizeRange)
+			l.resources = append(l.resources, resource)
+			pvcs = append(pvcs, resource.Pvc)
+		}
+		seed := time.Now().UTC().UnixNano()
+		ginkgo.By("Checking read/write works with empty buckets")
+		testConcurrentAccessToSingleVolume(ctx, pvcs, seed, true /* doWrite */)
+		ginkgo.By("Checking read works from non-empty buckets after pod recreation")
+		testConcurrentAccessToSingleVolume(ctx, pvcs, seed, false /* doWrite */)
+	})
 }
 
-// genBinDataFromSeed generate binData
-func genBinDataFromSeed(len int) []byte {
+// genBinDataFromSeed generate binData with random seed
+func genBinDataFromSeed(len int, seed int64) []byte {
 	binData := make([]byte, len)
+	rand.Seed(seed)
 
 	_, err := rand.Read(binData)
 	if err != nil {
@@ -160,17 +210,16 @@ func genBinDataFromSeed(len int) []byte {
 	return binData
 }
 
-func checkWriteToPath(f *framework.Framework, pod *v1.Pod, path string) []byte {
-	toWrite := 1024 // 1KB
-	data := genBinDataFromSeed(toWrite)
+func checkWriteToPath(f *framework.Framework, pod *v1.Pod, path string, toWrite int, seed int64) {
+	data := genBinDataFromSeed(toWrite, seed)
 	encoded := base64.StdEncoding.EncodeToString(data)
 	e2evolume.VerifyExecInPodSucceed(f, pod, fmt.Sprintf("echo %s | base64 -d | sha256sum", encoded))
-	e2evolume.VerifyExecInPodSucceed(f, pod, fmt.Sprintf("echo %s | base64 -d | dd of=%s bs=%d count=1", encoded, path, len(data)))
-	return data
+	e2evolume.VerifyExecInPodSucceed(f, pod, fmt.Sprintf("echo %s | base64 -d | dd of=%s bs=%d count=1", encoded, path, toWrite))
+	ginkgo.By(fmt.Sprintf("written data with sha: %x", sha256.Sum256(data)))
 }
 
-func checkReadFromPath(f *framework.Framework, pod *v1.Pod, path string, data []byte) {
-	sum := sha256.Sum256(data)
-	e2evolume.VerifyExecInPodSucceed(f, pod, fmt.Sprintf("dd if=%s bs=%d count=1 | sha256sum", path, len(data)))
-	e2evolume.VerifyExecInPodSucceed(f, pod, fmt.Sprintf("dd if=%s bs=%d count=1 | sha256sum | grep -Fq %x", path, len(data), sum))
+func checkReadFromPath(f *framework.Framework, pod *v1.Pod, path string, toWrite int, seed int64) {
+	sum := sha256.Sum256(genBinDataFromSeed(toWrite, seed))
+	e2evolume.VerifyExecInPodSucceed(f, pod, fmt.Sprintf("dd if=%s bs=%d count=1 | sha256sum", path, toWrite))
+	e2evolume.VerifyExecInPodSucceed(f, pod, fmt.Sprintf("dd if=%s bs=%d count=1 | sha256sum | grep -Fq %x", path, toWrite, sum))
 }
