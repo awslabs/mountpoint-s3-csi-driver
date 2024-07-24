0b126fe30576edeeae0ef3bac926c259e056e8af
Vladislav Volodkin <vlaad@amazon.co.uk>
2024-07-24 22:44:21 +0000

    Remove SG after ENI, do not remove default SG
    Signed-off-by: Vladislav Volodkin <vlaad@amazon.co.uk>

--- tests/e2e-kubernetes/scripts/eksctl.sh
+++ tests/e2e-kubernetes/scripts/eksctl.sh
@@ -68,18 +68,19 @@ function eksctl_delete_cluster() {
       aws ec2 delete-vpc-endpoints --region ${REGION} --vpc-endpoint-ids ${ENDPOINT}
     fi
 
-    SECURITY_GROUP=$(aws ec2 describe-security-groups --region ${REGION} | jq -r --arg VPC_ID "$VPC_ID" '.["SecurityGroups"][] | select(.["VpcId"]==$VPC_ID) | .["GroupId"]')
-    if [ -n "$SECURITY_GROUP" ]; then
-      # security group deletion only succeeds after a certain step of stack deletion was passed (namely subnets deletion),
-      # after which stack deletion is blocked because of the security group, so we retry here until this step is completed
-      delete_security_group ${REGION} ${SECURITY_GROUP}
-    fi
-
+    # https://github.com/eksctl-io/eksctl/issues/7589
     ENIS=$(aws ec2 describe-network-interfaces --region ${REGION} | jq -r --arg VPC_ID "$VPC_ID" '.["NetworkInterfaces"][] | select(.["VpcId"]==$VPC_ID) | .NetworkInterfaceId')
     if [ -n "$ENIS" ]; then
       echo "${ENIS}" | while IFS= read -r ENI_ID ; do
         delete_eni ${REGION} ${ENI_ID}
       done
+    fi
+
+    SECURITY_GROUP=$(aws ec2 describe-security-groups --region ${REGION} | jq -r --arg VPC_ID "$VPC_ID" '.["SecurityGroups"][] | select(.["VpcId"]==$VPC_ID and .["GroupName"]!="default") | .["GroupId"]')
+    if [ -n "$SECURITY_GROUP" ]; then
+      # security group deletion only succeeds after a certain step of stack deletion was passed (namely subnets deletion),
+      # after which stack deletion is blocked because of the security group, so we retry here until this step is completed
+      delete_security_group ${REGION} ${SECURITY_GROUP}
     fi
   fi
 
@@ -103,7 +104,7 @@ function delete_security_group() {
   REGION=${1}
   SECURITY_GROUP=${2}
 
-  remaining_attemps=10
+  remaining_attemps=20
   while (( remaining_attemps-- > 0 ))
   do
       if $(aws ec2 delete-security-group --region ${REGION} --group-id ${SECURITY_GROUP}); then
@@ -117,10 +118,13 @@ function delete_eni() {
   REGION=${1}
   ENI_ID=${2}
 
-  remaining_attemps=10
+  remaining_attemps=20
   while (( remaining_attemps-- > 0 ))
   do
-      if $(aws ec2 delete-network-interface --network-interface-id ${ENI_ID} --region ${REGION}); then
+      if output=$(aws ec2 delete-network-interface --network-interface-id ${ENI_ID} --region ${REGION} 2>&1); then
+        return
+      fi
+      if [[ $output == *"InvalidNetworkInterfaceID.NotFound"* ]]; then
         return
       fi
       sleep 30
