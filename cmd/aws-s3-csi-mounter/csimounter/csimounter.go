7d574b079c3d99be3c2bba0559fb66070ded542b
Burak Varlı <burakvar@amazon.co.uk>
2024-11-11 16:50:01 +0000

    Add `aws-s3-csi-mounter` command
    Signed-off-by: Burak Varlı <burakvar@amazon.co.uk>

---
+++ cmd/aws-s3-csi-mounter/csimounter/csimounter.go
@@ -0,0 +1,69 @@
+package csimounter
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"slices"
+
+	"github.com/awslabs/aws-s3-csi-driver/pkg/podmounter/mountoptions"
+)
+
+// A CmdRunner is responsible for running given `cmd` until completion and returning its exit code and its error (if any).
+// This is mainly exposed for mocking in tests, `DefaultCmdRunner` is always used in non-test environments.
+type CmdRunner func(cmd *exec.Cmd) (int, error)
+
+// DefaultCmdRunner is a real CmdRunner implementation that runs given `cmd`.
+func DefaultCmdRunner(cmd *exec.Cmd) (int, error) {
+	err := cmd.Run()
+	if err != nil {
+		return 0, err
+	}
+	return cmd.ProcessState.ExitCode(), nil
+}
+
+// An Options represents options to use while mounting Mountpoint.
+type Options struct {
+	MountpointPath string
+	MountOptions   mountoptions.Options
+	CmdRunner      CmdRunner
+}
+
+// Run runs Mountpoint with given options until completion and returns its exit code and its error (if any).
+func Run(options Options) (int, error) {
+	if options.CmdRunner == nil {
+		options.CmdRunner = DefaultCmdRunner
+	}
+
+	mountOptions := options.MountOptions
+
+	fuseDev := os.NewFile(uintptr(mountOptions.Fd), "/dev/fuse")
+	if fuseDev == nil {
+		return 0, fmt.Errorf("passed file descriptor %d is invalid", mountOptions.Fd)
+	}
+
+	args := mountOptions.Args
+
+	// By default Mountpoint runs in a detached mode. Here we want to monitor it by relaying its output,
+	// and also we want to wait until it terminates. We're passing `--foreground` to achieve this.
+	const foreground = "--foreground"
+	if !slices.Contains(args, foreground) {
+		args = append(args, foreground)
+	}
+
+	args = append([]string{
+		mountOptions.BucketName,
+		// We pass FUSE fd using `ExtraFiles`, and each entry becomes as file descriptor 3+i.
+		"/dev/fd/3",
+	}, args...)
+
+	cmd := exec.Command(options.MountpointPath, args...)
+	cmd.ExtraFiles = []*os.File{fuseDev}
+	cmd.Env = options.MountOptions.Env
+	// Connect Mountpoint's stdout/stderr to this commands stdout/stderr,
+	// so Mountpoint logs can be viewable with `kubectl logs`.
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return options.CmdRunner(cmd)
+}
---
+++ cmd/aws-s3-csi-mounter/main.go
@@ -0,0 +1,53 @@
+// WIP: Part of https://github.com/awslabs/mountpoint-s3-csi-driver/issues/279.
+//
+// `aws-s3-csi-mounter` is the entrypoint binary running on Mountpoint Pods.
+// It is responsible for receiving mount options from the CSI Driver Node Pod,
+// and spawning a Mountpoint instance in turn.
+// It will then wait until Mountpoint process terminates (which normally happens as a result of `unmount`).
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"path/filepath"
+	"time"
+
+	"k8s.io/klog/v2"
+
+	"github.com/awslabs/aws-s3-csi-driver/cmd/aws-s3-csi-mounter/csimounter"
+	"github.com/awslabs/aws-s3-csi-driver/pkg/podmounter/mountoptions"
+)
+
+var mountSockPath = flag.String("mount-sock-path", "/sock/mount.sock", "Path of the Unix socket to receive mount options from.")
+var mountSockRecvTimeout = flag.Duration("mount-sock-recv-timeout", 2*time.Minute, "Timeout for receiving mount options from passed Unix socket.")
+var mountpointBinDir = flag.String("mountpoint-bin-dir", os.Getenv("MOUNTPOINT_BIN_DIR"), "Directory of mount-s3 binary.")
+
+const mountpointBin = "mount-s3"
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	mountpointBinFullPath := filepath.Join(*mountpointBinDir, mountpointBin)
+	mountOptions := recvMountOptions()
+
+	exitCode, err := csimounter.Run(csimounter.Options{
+		MountpointPath: mountpointBinFullPath,
+		MountOptions:   mountOptions,
+	})
+	if err != nil {
+		klog.Fatalf("Failed to run Mountpoint: %v\n", err)
+	}
+	os.Exit(exitCode)
+}
+
+func recvMountOptions() mountoptions.Options {
+	ctx, cancel := context.WithTimeout(context.Background(), *mountSockRecvTimeout)
+	defer cancel()
+	options, err := mountoptions.Recv(ctx, *mountSockPath)
+	if err != nil {
+		klog.Fatalf("Failed to receive mount options from %s: %v\n", *mountSockPath, err)
+	}
+	return options
+}
