3f53d4758bde2b6e10ec958c26e084fa4e3f556a
Burak Varlı <burakvar@amazon.co.uk>
2024-11-11 11:27:00 +0000

    Add test util `assert` package
    Signed-off-by: Burak Varlı <burakvar@amazon.co.uk>

---
+++ pkg/util/testutil/assert/assert.go
@@ -0,0 +1,25 @@
+// Package assert provides utilities for making assertions during tests.
+package assert
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+// Equals fails the test if `want` and `got` are not equal.
+func Equals[T interface{}](t *testing.T, want T, got T) {
+	t.Helper()
+	if diff := cmp.Diff(want, got, cmpopts.EquateErrors()); diff != "" {
+		t.Errorf("Assertion failure (-want +got):\n%s", diff)
+	}
+}
+
+// NoError fails the test if `err` is not nil.
+func NoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("Expected no error, but got: %s", err)
+	}
+}
