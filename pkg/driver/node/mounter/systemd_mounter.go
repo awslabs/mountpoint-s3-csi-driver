4c7f236427571c23a46ac50b49149e714c9cdcc7
Burak Varlı <burakvar@amazon.co.uk>
2024-12-27 17:13:09 +0000

    Use newly created packages in `SystemdMounter`
    Signed-off-by: Burak Varlı <burakvar@amazon.co.uk>

--- pkg/driver/node/mounter/systemd_mounter.go
+++ pkg/driver/node/mounter/systemd_mounter.go
@@ -2,44 +2,48 @@ package mounter
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
-	"github.com/awslabs/aws-s3-csi-driver/pkg/driver/node/awsprofile"
-	"github.com/awslabs/aws-s3-csi-driver/pkg/system"
 	"github.com/google/uuid"
 	"k8s.io/klog/v2"
 	"k8s.io/mount-utils"
+
+	"github.com/awslabs/aws-s3-csi-driver/pkg/driver/node/credentialprovider"
+	"github.com/awslabs/aws-s3-csi-driver/pkg/driver/node/envprovider"
+	"github.com/awslabs/aws-s3-csi-driver/pkg/mountpoint"
+	"github.com/awslabs/aws-s3-csi-driver/pkg/system"
 )
+
+var defaultMountS3Path = "/usr/bin/mount-s3"
 
 // https://github.com/awslabs/mountpoint-s3/blob/9ed8b6243f4511e2013b2f4303a9197c3ddd4071/mountpoint-s3/src/cli.rs#L421
 const mountpointDeviceName = "mountpoint-s3"
 
 type SystemdMounter struct {
-	Ctx               context.Context
-	Runner            ServiceRunner
-	Mounter           mount.Interface
-	MpVersion         string
-	MountS3Path       string
-	kubernetesVersion string
+	Ctx         context.Context
+	Runner      ServiceRunner
+	Mounter     mount.Interface
+	MpVersion   string
+	MountS3Path string
 }
 
-func NewSystemdMounter(mpVersion string, kubernetesVersion string) (*SystemdMounter, error) {
+func NewSystemdMounter(mpVersion string) (*SystemdMounter, error) {
 	ctx := context.Background()
 	runner, err := system.StartOsSystemdSupervisor()
 	if err != nil {
 		return nil, fmt.Errorf("failed to start systemd supervisor: %w", err)
 	}
 	return &SystemdMounter{
-		Ctx:               ctx,
-		Runner:            runner,
-		Mounter:           mount.New(""),
-		MpVersion:         mpVersion,
-		MountS3Path:       MountS3Path(),
-		kubernetesVersion: kubernetesVersion,
+		Ctx:         ctx,
+		Runner:      runner,
+		Mounter:     mount.New(""),
+		MpVersion:   mpVersion,
+		MountS3Path: MountS3Path(),
 	}, nil
 }
 
@@ -79,7 +83,7 @@ func (m *SystemdMounter) IsMountPoint(target string) (bool, error) {
 //
 // This method will create the target path if it does not exist and if there is an existing corrupt
 // mount, it will attempt an unmount before attempting the mount.
-func (m *SystemdMounter) Mount(bucketName string, target string, credentials *MountCredentials, options []string) error {
+func (m *SystemdMounter) Mount(bucketName string, target string, credentials credentialprovider.Credentials, env envprovider.Environment, args mountpoint.Args) error {
 	if bucketName == "" {
 		return fmt.Errorf("bucket name is empty")
 	}
@@ -122,39 +126,31 @@ func (m *SystemdMounter) Mount(bucketName string, target string, credentials *Mo
 		return fmt.Errorf("Could not check if %q is a mount point: %v, %v", target, statErr, err)
 	}
 
+	credentialsBasepath, err := m.ensureCredentialsDirExists(target)
+	if err != nil {
+		return err
+	}
+
+	// Note that this part happens before `isMountPoint` check, as we want to update credentials even though
+	// there is an existing mount point at `target`.
+	credentialsEnv, err := credentials.Dump(credentialsBasepath, credentialsBasepath)
+	if err != nil {
+		klog.V(4).Infof("NodePublishVolume: Failed to dump credentials for %s: %v", target, err)
+		return err
+	}
+
+	env = append(env, credentialsEnv...)
+
 	if isMountPoint {
 		klog.V(4).Infof("NodePublishVolume: Target path %q is already mounted", target)
 		return nil
 	}
 
-	env := []string{}
-	var authenticationSource AuthenticationSource
-	if credentials != nil {
-		var awsProfile awsprofile.AWSProfile
-		if credentials.AccessKeyID != "" && credentials.SecretAccessKey != "" {
-			// Kubernetes creates target path in the form of "/var/lib/kubelet/pods/<pod-uuid>/volumes/kubernetes.io~csi/<volume-id>/mount".
-			// So the directory of the target path is unique for this mount, and we can use it to write credentials and config files.
-			// These files will be cleaned up in `Unmount`.
-			basepath := filepath.Dir(target)
-			awsProfile, err = awsprofile.CreateAWSProfile(basepath, credentials.AccessKeyID, credentials.SecretAccessKey, credentials.SessionToken)
-			if err != nil {
-				klog.V(4).Infof("Mount: Failed to create AWS Profile in %s: %v", basepath, err)
-				return fmt.Errorf("Mount: Failed to create AWS Profile in %s: %v", basepath, err)
-			}
-		}
-
-		authenticationSource = credentials.AuthenticationSource
-
-		env = credentials.Env(awsProfile)
-	}
-	options, env = moveOptionToEnvironmentVariables(awsMaxAttemptsOption, awsMaxAttemptsEnv, options, env)
-	options = addUserAgentToOptions(options, UserAgent(authenticationSource, m.kubernetesVersion))
-
 	output, err := m.Runner.StartService(timeoutCtx, &system.ExecConfig{
 		Name:        "mount-s3-" + m.MpVersion + "-" + uuid.New().String() + ".service",
 		Description: "Mountpoint for Amazon S3 CSI driver FUSE daemon",
 		ExecPath:    m.MountS3Path,
-		Args:        append(options, bucketName, target),
+		Args:        append(args.SortedList(), bucketName, target),
 		Env:         env,
 	})
 
@@ -168,47 +164,9 @@ func (m *SystemdMounter) Mount(bucketName string, target string, credentials *Mo
 	return nil
 }
 
-// Moves a parameter optionName from the options list to MP's environment variable list. We need this as options are
-// passed to the driver in a single field, but MP sometimes only supports config from environment variables.
-// Returns an updated options and environment.
-func moveOptionToEnvironmentVariables(optionName string, envName string, options []string, env []string) ([]string, []string) {
-	optionIdx := -1
-	for i, o := range options {
-		if strings.HasPrefix(o, optionName) {
-			optionIdx = i
-			break
-		}
-	}
-	if optionIdx != -1 {
-		// We can do replace here as we've just verified it has the right prefix
-		env = append(env, strings.Replace(options[optionIdx], optionName, envName, 1))
-		options = append(options[:optionIdx], options[optionIdx+1:]...)
-	}
-	return options, env
-}
-
-// method to add the user agent prefix to the Mountpoint headers
-// https://github.com/awslabs/mountpoint-s3/pull/548
-func addUserAgentToOptions(options []string, userAgent string) []string {
-	// first remove it from the options in case it's in there
-	for i := len(options) - 1; i >= 0; i-- {
-		if strings.Contains(options[i], userAgentPrefix) {
-			options = append(options[:i], options[i+1:]...)
-		}
-	}
-	// add the hard coded S3 CSI driver user agent string
-	return append(options, userAgentPrefix+"="+userAgent)
-}
-
 func (m *SystemdMounter) Unmount(target string) error {
 	timeoutCtx, cancel := context.WithTimeout(m.Ctx, 30*time.Second)
 	defer cancel()
-
-	basepath := filepath.Dir(target)
-	err := awsprofile.CleanupAWSProfile(basepath)
-	if err != nil {
-		klog.V(4).Infof("Unmount: Failed to clean up AWS Profile in %s: %v", basepath, err)
-	}
 
 	output, err := m.Runner.RunOneshot(timeoutCtx, &system.ExecConfig{
 		Name:        "mount-s3-umount-" + uuid.New().String() + ".service",
@@ -222,23 +180,34 @@ func (m *SystemdMounter) Unmount(target string) error {
 	if output != "" {
 		klog.V(5).Infof("umount output: %s", output)
 	}
+
+	credentialsBasepath := m.credentialsDir(target)
+	err = os.RemoveAll(credentialsBasepath)
+	if err != nil {
+		klog.V(5).Infof("NodePublishVolume: Failed to clean up credentials for %s: %v", target, err)
+		return nil
+	}
+
 	return nil
 }
 
-const (
-	mountpointArgRegion = "region"
-	mountpointArgCache  = "cache"
-)
-
-// ExtractMountpointArgument extracts value of a given argument from `mountpointArgs`.
-func ExtractMountpointArgument(mountpointArgs []string, argument string) (string, bool) {
-	// `mountpointArgs` normalized to `--arg=val` in `S3NodeServer.NodePublishVolume`.
-	prefix := fmt.Sprintf("--%s=", argument)
-	for _, arg := range mountpointArgs {
-		if strings.HasPrefix(arg, prefix) {
-			val := strings.SplitN(arg, "=", 2)[1]
-			return val, true
-		}
+// ensureCredentialsDirExists ensures credentials dir for `target` is exists.
+// It returns credentials dir and any error.
+func (m *SystemdMounter) ensureCredentialsDirExists(target string) (string, error) {
+	credentialsBasepath := m.credentialsDir(target)
+	if err := os.Mkdir(credentialsBasepath, credentialprovider.CredentialDirPerm); !errors.Is(err, fs.ErrExist) {
+		klog.V(4).Infof("NodePublishVolume: Failed to create credentials directory for %s: %v", target, err)
+		return "", err
 	}
-	return "", false
+
+	return credentialsBasepath, nil
+}
+
+// credentialsDir returns a directory to write credentials to for given `target`.
+//
+// Kubernetes creates target path in the form of "/var/lib/kubelet/pods/<pod-uuid>/volumes/kubernetes.io~csi/<volume-id>/mount".
+// So, the directory of the target path is unique for this mount, and we're creating a new folder in this path for credentials.
+// The credentials folder and all its contents will be cleaned up in `Unmount`.
+func (m *SystemdMounter) credentialsDir(target string) string {
+	return filepath.Join(filepath.Dir(target), "credentials")
 }
