0c84df1a5372e77fa3544f150b9d17a8f26f3cf3
Burak Varlı <burakvar@amazon.co.uk>
2025-02-18 17:08:48 +0000

    Create `mppod/watcher` package for watching Mountpoint Pods

---
+++ pkg/podmounter/mppod/watcher/watcher.go
@@ -0,0 +1,122 @@
+// Package watcher provides utilities for watching Mountpoint Pods in the cluster.
+package watcher
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync/atomic"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	listerv1 "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+// ErrPodNotFound returned when the Mountpoint Pod could not be found in the cluster.
+var ErrPodNotFound = errors.New("mppod/watcher: mountpoint pod not found")
+
+// ErrPodNotReady returned when the Mountpoint Pod was found but is not ready.
+var ErrPodNotReady = errors.New("mppod/watcher: mountpoint pod not ready")
+
+// ErrCacheDesync returned when the Pod informer cache failed to synchronize within the specified timeout.
+var ErrCacheDesync = errors.New("mppod/watcher: failed to sync pod informer cache within the timeout")
+
+// Watcher provides functionality to watch and wait for Mountpoint Pods in the cluster.
+// It uses the Kubernetes informer to watch and cache Pod events.
+type Watcher struct {
+	informer cache.SharedIndexInformer
+	lister   listerv1.PodNamespaceLister
+}
+
+// New creates a new [Watcher] with the given Kubernetes client, Mountpoint Pod namespace, and resync duration.
+func New(client kubernetes.Interface, namespace string, defaultResync time.Duration) *Watcher {
+	factory := informers.NewSharedInformerFactoryWithOptions(client, defaultResync, informers.WithNamespace(namespace))
+	informer := factory.Core().V1().Pods().Informer()
+	lister := factory.Core().V1().Pods().Lister().Pods(namespace)
+	return &Watcher{informer, lister}
+}
+
+// Start begins watching for Pod events in the cluster.
+// It returns [ErrCacheDesync] if the informer cache fails to sync before [stopCh] is cancalled.
+// The provided [stopCh] can be used to stop the watching process.
+func (w *Watcher) Start(stopCh <-chan struct{}) error {
+	go w.informer.Run(stopCh)
+	if !cache.WaitForCacheSync(stopCh, w.informer.HasSynced) {
+		return ErrCacheDesync
+	}
+	return nil
+}
+
+// Wait blocks until the specified Mountpoint Pod is found and ready, or until the context is cancelled.
+func (w *Watcher) Wait(ctx context.Context, name string) (*corev1.Pod, error) {
+	// Set a watcher for Pod create & update events
+	var podFound atomic.Bool
+	podChan := make(chan *corev1.Pod, 1)
+	handle, err := w.informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			pod := obj.(*corev1.Pod)
+			if pod.Name == name {
+				podFound.Store(true)
+				if w.isPodReady(pod) {
+					podChan <- pod
+				}
+			}
+		},
+		UpdateFunc: func(old, new interface{}) {
+			pod := new.(*corev1.Pod)
+			if pod.Name == name {
+				podFound.Store(true)
+				if w.isPodReady(pod) {
+					podChan <- pod
+				}
+			}
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to add event handler for %s: %w", name, err)
+	}
+
+	// Ensure to remove event handler at the end
+	defer w.informer.RemoveEventHandler(handle)
+
+	// Check if the Pod already exists
+	pod, err := w.lister.Get(name)
+	if err == nil {
+		podFound.Store(true)
+		if w.isPodReady(pod) {
+			// Pod already exists and ready
+			return pod, nil
+		}
+	}
+
+	if err != nil && !apierrors.IsNotFound(err) {
+		// We got a different error than "not found", just propagate it
+		return nil, fmt.Errorf("failed to get pod %s: %w", name, err)
+	}
+
+	// Pod does not exists or not ready yet. We set a watcher for create & update events,
+	// and will receive the Pod from `podChan` once its ready.
+	select {
+	case pod := <-podChan:
+		// Pod found and ready
+		return pod, nil
+	case <-ctx.Done():
+		// We didn't received the Pod within the timeout
+
+		if podFound.Load() {
+			// Pod was found, but was not ready
+			return nil, ErrPodNotReady
+		}
+
+		return nil, ErrPodNotFound
+	}
+}
+
+// isPodReady returns whether the given Mountpoint Pod is ready.
+func (w *Watcher) isPodReady(pod *corev1.Pod) bool {
+	return pod.Status.Phase == corev1.PodRunning
+}
---
+++ pkg/podmounter/mppod/watcher/watcher_test.go
@@ -0,0 +1,154 @@
+package watcher_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+
+	"github.com/awslabs/aws-s3-csi-driver/pkg/podmounter/mppod/watcher"
+	"github.com/awslabs/aws-s3-csi-driver/pkg/util/testutil/assert"
+)
+
+func TestGettingAlreadyScheduledAndReadyPod(t *testing.T) {
+	client := fake.NewClientset()
+
+	mpPod := createMountpointPod(t, client, testMountpointPodName)
+	mpPod.run()
+
+	mpPodWatcher := createAndStartWatcher(t, client)
+
+	pod, err := mpPodWatcher.Wait(context.Background(), mpPod.pod.Name)
+	assert.NoError(t, err)
+	assert.Equals(t, mpPod.pod, pod)
+}
+
+func TestGettingScheduledButNotYetReadyPod(t *testing.T) {
+	client := fake.NewClientset()
+
+	mpPod := createMountpointPod(t, client, testMountpointPodName)
+
+	mpPodWatcher := createAndStartWatcher(t, client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	pod, err := mpPodWatcher.Wait(ctx, testMountpointPodName)
+	assert.Equals(t, watcher.ErrPodNotReady, err)
+	if pod != nil {
+		t.Fatalf("Pod should be nil if `watcher.ErrPodNotReady` error returned, but got %#v", pod)
+	}
+
+	mpPod.run()
+
+	pod, err = mpPodWatcher.Wait(context.Background(), testMountpointPodName)
+	assert.NoError(t, err)
+	assert.Equals(t, mpPod.pod, pod)
+}
+
+func TestGettingNotYetScheduledPod(t *testing.T) {
+	client := fake.NewClientset()
+
+	mpPodWatcher := createAndStartWatcher(t, client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	pod, err := mpPodWatcher.Wait(ctx, testMountpointPodName)
+	assert.Equals(t, watcher.ErrPodNotFound, err)
+	if pod != nil {
+		t.Fatalf("Pod should be nil if `watcher.ErrPodNotFound` error returned, but got %#v", pod)
+	}
+
+	mpPod := createMountpointPod(t, client, testMountpointPodName)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	pod, err = mpPodWatcher.Wait(ctx, testMountpointPodName)
+	assert.Equals(t, watcher.ErrPodNotReady, err)
+	if pod != nil {
+		t.Fatalf("Pod should be nil if `watcher.ErrPodNotReady` error returned, but got %#v", pod)
+	}
+
+	mpPod.run()
+
+	pod, err = mpPodWatcher.Wait(context.Background(), testMountpointPodName)
+	assert.NoError(t, err)
+	assert.Equals(t, mpPod.pod, pod)
+}
+
+func TestGettingPodsConcurrently(t *testing.T) {
+	client := fake.NewClientset()
+
+	mpPodWatcher := createAndStartWatcher(t, client)
+
+	foundPods := make(chan *corev1.Pod)
+	for i := 0; i < 5; i++ {
+		go func() {
+			pod, err := mpPodWatcher.Wait(context.Background(), testMountpointPodName)
+			assert.NoError(t, err)
+			foundPods <- pod
+		}()
+	}
+
+	mpPod := createMountpointPod(t, client, testMountpointPodName)
+	mpPod.run()
+
+	for i := 0; i < 5; i++ {
+		foundPod := <-foundPods
+		assert.Equals(t, mpPod.pod, foundPod)
+	}
+}
+
+func createAndStartWatcher(t *testing.T, client kubernetes.Interface) *watcher.Watcher {
+	mpPodWatcher := watcher.New(client, testMountpointPodNamespace, 10*time.Second)
+
+	stopCh := make(chan struct{})
+	t.Cleanup(func() {
+		close(stopCh)
+	})
+
+	err := mpPodWatcher.Start(stopCh)
+	assert.NoError(t, err)
+
+	return mpPodWatcher
+}
+
+const testMountpointPodName = "mp-pod"
+const testMountpointPodNamespace = "mount-s3-test"
+
+type mountpointPod struct {
+	t      *testing.T
+	client kubernetes.Interface
+	pod    *corev1.Pod
+}
+
+func createMountpointPod(t *testing.T, client kubernetes.Interface, name string) *mountpointPod {
+	t.Helper()
+
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			UID:  types.UID(uuid.New().String()),
+			Name: name,
+		},
+	}
+	pod, err := client.CoreV1().Pods(testMountpointPodNamespace).Create(context.Background(), pod, metav1.CreateOptions{})
+	assert.NoError(t, err)
+
+	return &mountpointPod{t, client, pod}
+}
+
+func (mp *mountpointPod) run() {
+	mp.t.Helper()
+	mp.pod.Status.Phase = corev1.PodRunning
+	var err error
+	mp.pod, err = mp.client.CoreV1().Pods(testMountpointPodNamespace).UpdateStatus(context.Background(), mp.pod, metav1.UpdateOptions{})
+	assert.NoError(mp.t, err)
+}
