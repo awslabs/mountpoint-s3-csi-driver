a1291d20a33e01e4f69328fff238090e7800f63d
Burak Varlı <burakvar@amazon.co.uk>
2025-02-20 11:51:11 +0000

    Run end-to-end tests in parallel using `ginkgo`

--- tests/e2e-kubernetes/scripts/run.sh
+++ tests/e2e-kubernetes/scripts/run.sh
@@ -201,7 +201,7 @@ elif [[ "${ACTION}" == "install_driver" ]]; then
 elif [[ "${ACTION}" == "run_tests" ]]; then
   set +e
   pushd tests/e2e-kubernetes
-  KUBECONFIG=${KUBECONFIG} go test -ginkgo.vv -timeout 30m --bucket-region=${REGION} --commit-id=${TAG} --bucket-prefix=${CLUSTER_NAME} --imds-available=true
+  KUBECONFIG=${KUBECONFIG} ginkgo -p -vv -timeout 30m -- --bucket-region=${REGION} --commit-id=${TAG} --bucket-prefix=${CLUSTER_NAME} --imds-available=true
   EXIT_CODE=$?
   print_cluster_info
   exit $EXIT_CODE
