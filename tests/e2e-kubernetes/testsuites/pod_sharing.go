87f4edb6e0869487f5ca85ff51ecc7b26a6647d1
Yerzhan Mazhkenov <20302932+yerzhan7@users.noreply.github.com>
2025-04-29 20:15:35 +0100

    Add more e2e tests

--- tests/e2e-kubernetes/testsuites/pod_sharing.go
+++ tests/e2e-kubernetes/testsuites/pod_sharing.go
@@ -79,7 +79,7 @@ func (t *s3CSIPodSharingTestSuite) DefineTests(driver storageframework.TestDrive
 		ginkgo.DeferCleanup(cleanup)
 	})
 
-	ginkgo.It("should concurrently access the single volume from pods on the same node using the same Mountpoint Pod", func(ctx context.Context) {
+	ginkgo.It("should share Mountpoint Pod (authenticationSource=driver)", func(ctx context.Context) {
 		resource := createVolumeResourceWithMountOptions(ctx, l.config, pattern, nil)
 		l.resources = append(l.resources, resource)
 
@@ -111,11 +111,56 @@ func (t *s3CSIPodSharingTestSuite) DefineTests(driver storageframework.TestDrive
 			verifyMountpointResourcesCleanup(ctx, f, s3paNames, mountpointPodNames)
 		}()
 
-		s3paNames, mountpointPodNames = verifyPodsShareMountpointPod(ctx, f, pods, resource.Pv)
+		s3paNames, mountpointPodNames = verifyPodsShareMountpointPod(ctx, f, pods, defaultExpectedFields(targetNode, resource.Pv))
 		checkCrossReadWrite(f, pods[0], pods[1])
 	})
 
-	ginkgo.It("should concurrently access the single volume from pods on the same node using different Mountpoint Pods if fsGroup is different", func(ctx context.Context) {
+	ginkgo.It("should share Mountpoint Pod if pods have the same fsGroup", func(ctx context.Context) {
+		resource := createVolumeResourceWithMountOptions(ctx, l.config, pattern, nil)
+		l.resources = append(l.resources, resource)
+
+		var s3paNames []string
+		var mountpointPodNames []string
+		var pods []*v1.Pod
+		var targetNode string
+		for i := 0; i < 2; i++ {
+			index := i + 1
+			podConfig := &e2epod.Config{
+				NS:            f.Namespace.Name,
+				PVCs:          []*v1.PersistentVolumeClaim{resource.Pvc},
+				SecurityLevel: admissionapi.LevelBaseline,
+				FsGroup:       ptr.To(int64(1000)),
+			}
+
+			if i > 0 && targetNode != "" {
+				podConfig.NodeSelection = e2epod.NodeSelection{
+					Name: targetNode,
+				}
+			}
+
+			ginkgo.By(fmt.Sprintf("Creating pod%d", index))
+			pod, err := e2epod.CreateSecPod(ctx, f.ClientSet, podConfig, 10*time.Second)
+			framework.ExpectNoError(err)
+
+			if i == 0 {
+				targetNode = pod.Spec.NodeName
+			}
+			pods = append(pods, pod)
+		}
+		defer func() {
+			for _, pod := range pods {
+				framework.ExpectNoError(e2epod.DeletePodWithWait(ctx, f.ClientSet, pod))
+			}
+			verifyMountpointResourcesCleanup(ctx, f, s3paNames, mountpointPodNames)
+		}()
+
+		expectedFields := defaultExpectedFields(targetNode, resource.Pv)
+		expectedFields["WorkloadFSGroup"] = "1000"
+		s3paNames, mountpointPodNames = verifyPodsShareMountpointPod(ctx, f, pods, expectedFields)
+		checkCrossReadWrite(f, pods[0], pods[1])
+	})
+
+	ginkgo.It("should not share Mountpoint Pod if pods have different fsGroup", func(ctx context.Context) {
 		resource := createVolumeResourceWithMountOptions(ctx, l.config, pattern, nil)
 		l.resources = append(l.resources, resource)
 
@@ -165,10 +210,152 @@ func (t *s3CSIPodSharingTestSuite) DefineTests(driver storageframework.TestDrive
 		checkCrossReadWrite(f, pods[0], pods[1])
 	})
 
-	// TODO: Add more test cases
+	ginkgo.It("should not share Mountpoint Pod if mountOptions are different", func(ctx context.Context) {
+		resource := createVolumeResourceWithMountOptions(ctx, l.config, pattern, nil)
+		l.resources = append(l.resources, resource)
+
+		var s3paNames []string
+		var mountpointPodNames []string
+		var pods []*v1.Pod
+		var targetNode string
+
+		// First Pod
+		ginkgo.By("Creating pod1 with a volume")
+		pod1, err := e2epod.CreatePod(ctx, f.ClientSet, f.Namespace.Name, nil, []*v1.PersistentVolumeClaim{resource.Pvc}, admissionapi.LevelBaseline, "")
+		framework.ExpectNoError(err)
+		targetNode = pod1.Spec.NodeName
+
+		resource.Pv, err = f.ClientSet.CoreV1().PersistentVolumes().Get(ctx, resource.Pv.Name, metav1.GetOptions{})
+		framework.ExpectNoError(err)
+		firstMountOptions := strings.Join(resource.Pv.Spec.MountOptions, ",")
+		resource.Pv.Spec.MountOptions = []string{"--allow-delete"}
+		resource.Pv, err = f.ClientSet.CoreV1().PersistentVolumes().Update(ctx, resource.Pv, metav1.UpdateOptions{})
+		framework.ExpectNoError(err)
+
+		// Second Pod
+		pods = append(pods, pod1)
+		ginkgo.By("Creating pod2 with a volume")
+		pod2, err := e2epod.CreatePod(ctx, f.ClientSet, f.Namespace.Name, map[string]string{"kubernetes.io/hostname": targetNode}, []*v1.PersistentVolumeClaim{resource.Pvc}, admissionapi.LevelBaseline, "")
+		framework.ExpectNoError(err)
+		pods = append(pods, pod2)
+
+		defer func() {
+			for _, pod := range pods {
+				framework.ExpectNoError(e2epod.DeletePodWithWait(ctx, f.ClientSet, pod))
+			}
+			verifyMountpointResourcesCleanup(ctx, f, s3paNames, mountpointPodNames)
+		}()
+
+		s3paNames, mountpointPodNames = verifyPodsHaveDifferentMountpointPods(ctx, f, pods, func(pod *v1.Pod) map[string]string {
+			expectedFields := defaultExpectedFields(pod.Spec.NodeName, resource.Pv)
+			if pod.Name == pod1.Name {
+				expectedFields["MountOptions"] = firstMountOptions
+			} else {
+				expectedFields["MountOptions"] = "--allow-delete"
+			}
+			return expectedFields
+		})
+		checkCrossReadWrite(f, pods[0], pods[1])
+	})
+
+	ginkgo.It("should share Mountpoint Pod if pod namespaces and service accounts are the same (authenticationSource=pod)", func(ctx context.Context) {
+		idConfig, err := setupPodLevelIdentity(ctx, f)
+		framework.ExpectNoError(err)
+		defer idConfig.Cleanup(ctx)
+		resource := createVolumeResourceWithMountOptions(contextWithAuthenticationSource(ctx, "pod"), l.config, pattern, nil)
+		l.resources = append(l.resources, resource)
+
+		var s3paNames []string
+		var mountpointPodNames []string
+		var pods []*v1.Pod
+		var targetNode string
+		var nodeSelector map[string]string
+		for i := 0; i < 2; i++ {
+			index := i + 1
+
+			if i > 0 && targetNode != "" {
+				nodeSelector = map[string]string{"kubernetes.io/hostname": targetNode}
+			}
+
+			ginkgo.By(fmt.Sprintf("Creating pod%d with a volume", index))
+			pod := e2epod.MakePod(f.Namespace.Name, nodeSelector, []*v1.PersistentVolumeClaim{resource.Pvc}, admissionapi.LevelBaseline, "")
+			pod.Spec.ServiceAccountName = idConfig.ServiceAccount.Name
+			pod, err := createPod(ctx, f.ClientSet, f.Namespace.Name, pod)
+			framework.ExpectNoError(err)
+
+			if i == 0 {
+				targetNode = pod.Spec.NodeName
+			}
+			pods = append(pods, pod)
+		}
+		defer func() {
+			for _, pod := range pods {
+				framework.ExpectNoError(e2epod.DeletePodWithWait(ctx, f.ClientSet, pod))
+			}
+			verifyMountpointResourcesCleanup(ctx, f, s3paNames, mountpointPodNames)
+		}()
+
+		expectedFields := defaultExpectedFields(targetNode, resource.Pv)
+		expectedFields["AuthenticationSource"] = "pod"
+		expectedFields["WorkloadNamespace"] = f.Namespace.Name
+		expectedFields["WorkloadServiceAccountName"] = idConfig.ServiceAccount.Name
+		s3paNames, mountpointPodNames = verifyPodsShareMountpointPod(ctx, f, pods, expectedFields)
+		checkCrossReadWrite(f, pods[0], pods[1])
+	})
+
+	ginkgo.It("should not share Mountpoint Pod if pod service accounts are the different (authenticationSource=pod)", func(ctx context.Context) {
+		idConfig1, err := setupPodLevelIdentity(ctx, f)
+		framework.ExpectNoError(err)
+		defer idConfig1.Cleanup(ctx)
+		idConfig2, err := setupPodLevelIdentity(ctx, f)
+		framework.ExpectNoError(err)
+		defer idConfig2.Cleanup(ctx)
+		saNames := []string{idConfig1.ServiceAccount.Name, idConfig2.ServiceAccount.Name}
+		resource := createVolumeResourceWithMountOptions(contextWithAuthenticationSource(ctx, "pod"), l.config, pattern, nil)
+		l.resources = append(l.resources, resource)
+
+		var s3paNames []string
+		var mountpointPodNames []string
+		var pods []*v1.Pod
+		var targetNode string
+		var nodeSelector map[string]string
+		for i := 0; i < 2; i++ {
+			index := i + 1
+
+			if i > 0 && targetNode != "" {
+				nodeSelector = map[string]string{"kubernetes.io/hostname": targetNode}
+			}
+
+			ginkgo.By(fmt.Sprintf("Creating pod%d with a volume", index))
+			pod := e2epod.MakePod(f.Namespace.Name, nodeSelector, []*v1.PersistentVolumeClaim{resource.Pvc}, admissionapi.LevelBaseline, "")
+			pod.Spec.ServiceAccountName = saNames[i]
+			pod, err := createPod(ctx, f.ClientSet, f.Namespace.Name, pod)
+			framework.ExpectNoError(err)
+
+			if i == 0 {
+				targetNode = pod.Spec.NodeName
+			}
+			pods = append(pods, pod)
+		}
+		defer func() {
+			for _, pod := range pods {
+				framework.ExpectNoError(e2epod.DeletePodWithWait(ctx, f.ClientSet, pod))
+			}
+			verifyMountpointResourcesCleanup(ctx, f, s3paNames, mountpointPodNames)
+		}()
+
+		s3paNames, mountpointPodNames = verifyPodsHaveDifferentMountpointPods(ctx, f, pods, func(pod *v1.Pod) map[string]string {
+			expectedFields := defaultExpectedFields(pod.Spec.NodeName, resource.Pv)
+			expectedFields["AuthenticationSource"] = "pod"
+			expectedFields["WorkloadNamespace"] = f.Namespace.Name
+			expectedFields["WorkloadServiceAccountName"] = pod.Spec.ServiceAccountName
+			return expectedFields
+		})
+		checkCrossReadWrite(f, pods[0], pods[1])
+	})
 }
 
-func verifyPodsShareMountpointPod(ctx context.Context, f *framework.Framework, pods []*v1.Pod, pv *v1.PersistentVolume) ([]string, []string) {
+func verifyPodsShareMountpointPod(ctx context.Context, f *framework.Framework, pods []*v1.Pod, expectedFields map[string]string) ([]string, []string) {
 	var s3paNames []string
 	var mountpointPodNames []string
 	var s3paList *crdv1beta.MountpointS3PodAttachmentList
@@ -182,7 +369,7 @@ func verifyPodsShareMountpointPod(ctx context.Context, f *framework.Framework, p
 			return false, err
 		}
 		for _, s3pa := range s3paList.Items {
-			if matchesSpec(s3pa.Spec, defaultExpectedFields(pods[0].Spec.NodeName, pv)) {
+			if matchesSpec(s3pa.Spec, expectedFields) {
 				s3paNames = append(s3paNames, s3pa.Name)
 				allUIDs := make(map[string]bool)
 				for mpPodName, uids := range s3pa.Spec.MountpointS3PodToWorkloadPodUIDs {
@@ -264,36 +451,38 @@ func verifyPodsHaveDifferentMountpointPods(ctx context.Context, f *framework.Fra
 	return s3paNames, mountpointPodNames
 }
 
+// TODO: This does not fail for some reason after timeout
 func verifyMountpointResourcesCleanup(ctx context.Context, f *framework.Framework, s3paNames []string, mountpointPodNames []string) {
-	// Verify specific MountpointS3PodAttachments are deleted
-	framework.Gomega().Eventually(ctx, framework.HandleRetry(func(ctx context.Context) (bool, error) {
+	framework.Logf("Verifying MountpointS3PodAttachments are deleted: %v", s3paNames)
+	framework.Gomega().Eventually(ctx, func() bool {
 		for _, s3paName := range s3paNames {
 			_, err := f.DynamicClient.Resource(s3paGVR).Get(ctx, s3paName, metav1.GetOptions{})
 			if err == nil {
 				// S3PodAttachment still exists
-				return false, nil
+				return false
 			}
 			if !apierrors.IsNotFound(err) {
-				return false, err
+				return false
 			}
 		}
-		return true, nil
-	})).WithTimeout(defaultTimeout).WithPolling(defaultInterval).Should(gomega.BeTrue())
+		return true
+	}).WithTimeout(defaultTimeout).WithPolling(defaultInterval).Should(gomega.BeTrue())
 
-	// Verify specific Mountpoint Pods are deleted
-	framework.Gomega().Eventually(ctx, framework.HandleRetry(func(ctx context.Context) (bool, error) {
+	framework.Logf("Verifying Mountpoint Pods are deleted: %v", mountpointPodNames)
+	framework.Gomega().Eventually(ctx, func() bool {
 		for _, mpPodName := range mountpointPodNames {
 			_, err := f.ClientSet.CoreV1().Pods(mountpointNamespace).Get(ctx, mpPodName, metav1.GetOptions{})
 			if err == nil {
 				// Pod still exists
-				return false, nil
+				return false
 			}
 			if !apierrors.IsNotFound(err) {
-				return false, err
+				framework.Logf("Error checking pod %s: %v", mpPodName, err)
+				return false
 			}
 		}
-		return true, nil
-	})).WithTimeout(defaultTimeout).WithPolling(defaultInterval).Should(gomega.BeTrue())
+		return true
+	}).WithTimeout(defaultTimeout).WithPolling(defaultInterval).Should(gomega.BeTrue())
 }
 
 // Convert UnstructuredList to MountpointS3PodAttachmentList
@@ -365,4 +554,57 @@ func checkPodWriteAndOtherPodRead(f *framework.Framework, writerPod, readerPod *
 
 	checkWriteToPath(f, writerPod, filePath, size, seed)
 	checkReadFromPath(f, readerPod, filePath, size, seed)
+}
+
+type PodLevelIdentityConfig struct {
+	OIDCProvider   string
+	ServiceAccount *v1.ServiceAccount
+	IAMRole        string
+	Cleanup        func(context.Context) error
+}
+
+// setupPodLevelIdentity creates necessary resources for pod-level identity tests
+func setupPodLevelIdentity(ctx context.Context, f *framework.Framework) (*PodLevelIdentityConfig, error) {
+	config := &PodLevelIdentityConfig{}
+	var cleanupFuncs []func(context.Context) error
+
+	// Get OIDC Provider
+	config.OIDCProvider = oidcProviderForCluster(ctx, f)
+	if config.OIDCProvider == "" {
+		return nil, fmt.Errorf("OIDC provider is not configured")
+	}
+
+	// Create Service Account
+	sa, removeSA := createServiceAccount(ctx, f)
+	config.ServiceAccount = sa
+	cleanupFuncs = append(cleanupFuncs, removeSA)
+
+	// Create IAM Role with full access policy
+	role, removeRole := createRole(ctx, f,
+		assumeRoleWithWebIdentityPolicyDocument(ctx, config.OIDCProvider, sa),
+		iamPolicyS3FullAccess)
+	config.IAMRole = *role.Arn
+	cleanupFuncs = append(cleanupFuncs, removeRole)
+
+	// Annotate Service Account with Role ARN
+	sa, restoreServiceAccountRole := overrideServiceAccountRole(ctx, f, sa, config.IAMRole)
+	config.ServiceAccount = sa
+	cleanupFuncs = append(cleanupFuncs, restoreServiceAccountRole)
+
+	// Wait for role to be assumable
+	waitUntilRoleIsAssumableWithWebIdentity(ctx, f, sa)
+
+	// Combine cleanup functions
+	config.Cleanup = func(ctx context.Context) error {
+		var errs []error
+		// Execute cleanup functions in reverse order
+		for i := len(cleanupFuncs) - 1; i >= 0; i-- {
+			if err := cleanupFuncs[i](ctx); err != nil {
+				errs = append(errs, err)
+			}
+		}
+		return errors.NewAggregate(errs)
+	}
+
+	return config, nil
 }
