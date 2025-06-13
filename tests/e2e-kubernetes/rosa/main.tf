332c9152f07c114a211caa7ddc9fb24dbf64a8d0
Yerzhan Mazhkenov <20302932+yerzhan7@users.noreply.github.com>
2025-06-13 16:33:05 +0100

    Remove not supported vpc arguments

--- tests/e2e-kubernetes/rosa/main.tf
+++ tests/e2e-kubernetes/rosa/main.tf
@@ -56,13 +56,4 @@ module "vpc" {
   source  = "terraform-redhat/rosa-hcp/rhcs//modules/vpc"
 
   name_prefix              = "${var.cluster_name}-vpc"
-  azs             = ["us-east-1a"]
-  cidr  = "10.0.0.0/16"
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = false
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 }
