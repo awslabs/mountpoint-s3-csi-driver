6ffd3823a23ecc05bf96234619db520fc921ee19
Burak Varlı <burakvar@amazon.co.uk>
2025-07-23 10:40:31 +0100

    Correctly pass environment variables to upgrade test
    Signed-off-by: Burak Varlı <burakvar@amazon.co.uk>

--- tests/e2e-kubernetes/testsuites/upgrade.go
+++ tests/e2e-kubernetes/testsuites/upgrade.go
@@ -41,8 +41,8 @@ const helmReleaseNamespace = "kube-system"
 
 var helmChartPreviousVersion = os.Getenv("MOUNTPOINT_CSI_DRIVER_PREVIOUS_VERSION")
 var helmChartNewVersion = os.Getenv("MOUNTPOINT_CSI_DRIVER_NEW_VERSION")
-var helmChartContainerRepository = os.Getenv("MOUNTPOINT_CSI_DRIVER_CONTAINER_REPOSITORY")
-var helmChartContainerTag = os.Getenv("MOUNTPOINT_CSI_DRIVER_CONTAINER_TAG")
+var helmChartContainerRepository = os.Getenv("REGISTRY")
+var helmChartContainerTag = os.Getenv("TAG")
 
 type s3CSIUpgradeTestSuite struct {
 	tsInfo storageframework.TestSuiteInfo
@@ -271,7 +271,7 @@ func (t *s3CSIUpgradeTestSuite) DefineTests(driver storageframework.TestDriver, 
 // packageHelmChartFromSource creates a Helm package from the CSI Driver's source.
 func packageHelmChartFromSource(version string) string {
 	if helmChartContainerRepository == "" || helmChartContainerTag == "" {
-		Fail("Please set container repository and tag using `MOUNTPOINT_CSI_DRIVER_CONTAINER_REPOSITORY` and `MOUNTPOINT_CSI_DRIVER_CONTAINER_TAG` environment variables if you want to test a source build")
+		Fail("Please set container repository and tag using `REGISTRY` and `TAG` environment variables if you want to test a source build")
 	}
 
 	out := GinkgoT().TempDir()
