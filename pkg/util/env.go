0e19332653419a03fe640036d5f694396bbcf94c
Jensen Tong <jetong@amazon.com>
2026-06-30 09:03:05 +0000

    feat(daemonset): support MaxVolumesPerNode
    - Advertise MaxVolumesPerNode in our NodeGetInfoResponse to allow maxVolumesPerNode configuration. Pods exceeding limit stays pending with reason FailedScheduling.
    - Force >=1 in helm to avoid misconfiguration (0 means unbounded, to see
      if we want to allow this)
    - GetEnvAsIntOrFallback addition copying kubernetes code (slightly changed from original var GetEnv approach;
    - Used 1Gi to avoid confusion with 10x2Gi example
    
    Signed-off-by: Jensen Tong <jetong@amazon.com>

--- pkg/util/env.go
+++ pkg/util/env.go
@@ -1,7 +1,24 @@
 package util
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 func SupportLegacySystemdMounts() bool {
 	return os.Getenv("SUPPORT_LEGACY_SYSTEMD_MOUNTS") == "true"
+}
+
+// GetEnvAsIntOrFallback returns the env variable (parsed as integer) for
+// the given key and falls back to the given defaultValue if not set.
+// Copied from https://github.com/kubernetes/kubernetes/blob/release-1.36/pkg/util/env/env.go
+func GetEnvAsIntOrFallback(key string, defaultValue int) (int, error) {
+	if v := os.Getenv(key); v != "" {
+		value, err := strconv.Atoi(v)
+		if err != nil {
+			return defaultValue, err
+		}
+		return value, nil
+	}
+	return defaultValue, nil
 }
